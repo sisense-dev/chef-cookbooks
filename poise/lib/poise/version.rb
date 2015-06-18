45c87ecf4a3ed5cda79d2168dc863b71b8247603
sisense-dev <liran.shani@sisense.com>
2015-06-18 09:21:50 +0300

    replace poise cookbook

--- poise/lib/poise/version.rb
+++ poise/lib/poise/version.rb
@@ -14,5 +14,7 @@
 # limitations under the License.
 #
 
-raise 'Halite is not compatible with no_lazy_load false, please set no_lazy_load true in your Chef configuration file.' unless Chef::Config[:no_lazy_load]
-$LOAD_PATH << File.expand_path('../../files/halite_gem', __FILE__)
+
+module Poise
+  VERSION = '2.0.2.pre'
+end
