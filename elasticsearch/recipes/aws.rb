ac2f3876c52d08a01040caba3a64ea1dddf07675
sisense-dev <liran.shani@sisense.com>
2015-06-18 15:06:25 +0300

    try to explicit download plugin

--- elasticsearch/recipes/aws.rb
+++ elasticsearch/recipes/aws.rb
@@ -1,3 +1,5 @@
-node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
+#node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
 
-install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
+#install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
+
+install_plugin 'cloud-aws', 'url' => 'https://github.com/elastic/elasticsearch-cloud-aws/master.zip'
