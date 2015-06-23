ec0b08dd766d8d72cfe31bd9ac7d2f0e9121994e
sisense-dev <liran.shani@sisense.com>
2015-06-23 07:19:54 +0300

    update

--- chamber-kibana/attributes/default.rb
+++ chamber-kibana/attributes/default.rb
@@ -4,8 +4,9 @@ default['kibana']['version']       = '4.1.0'
 # sha256 ( shasum -a 256 FILENAME )
 default['kibana']['checksum']      = 'd593af567c5dd814e59fb2bc2e11a194add6caa0f58c54c82da8fb708554aded'
 default['kibana']['host']          = 'http://download.elasticsearch.org'
-default['kibana']['repository']    = kibana/kibana
-default['kibana']['filename']      = nil
+default['kibana']['repository']    = 'kibana/kibana'
+default['kibana']['filename']   = nil
+default['kibana']['download_url']  =   nil 
 #default['kibana']['download_url']  = 'https://github.com/sisense-dev/kibana/archive/master.zip'
 default['kibana']['download_url'] = nil
 # === DEPENDENCIES
