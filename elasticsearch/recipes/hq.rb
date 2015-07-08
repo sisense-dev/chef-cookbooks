c1a7519fc7f14e70246abb0f916a018d1e66332d
sisense-dev <liran.shani@sisense.com>
2015-07-08 09:15:11 +0300

    fix

--- elasticsearch/recipes/hq.rb
+++ elasticsearch/recipes/hq.rb
@@ -1,8 +1,3 @@
 node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['hq'])
 
 install_plugin "royrusso/elasticsearch-HQ"
-
-service 'elasticsearch' do:
-	action :restart
-end
-
