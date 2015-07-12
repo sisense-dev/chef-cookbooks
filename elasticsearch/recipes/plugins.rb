c98827381aa998ea532a1f5cabf229edff9af36a
sisense-dev <liran.shani@sisense.com>
2015-07-12 10:15:42 +0300

    fix

--- elasticsearch/recipes/plugins.rb
+++ elasticsearch/recipes/plugins.rb
@@ -8,6 +8,5 @@ end
 node[:elasticsearch][:plugins].each do | name, config |
   next if name == 'elasticsearch/elasticsearch-cloud-aws' && !node.recipe?('aws')
   next if name == 'elasticsearch/elasticsearch-cloud-gce' && !node.recipe?('gce')
-  next if name == 'royrusso/elasticsearch-HQ'  && !node.recipe?('hq')
   install_plugin name, config
 end
