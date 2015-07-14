311bb0216cc58d3ff4a81696a229699bbedc9a62
sisense-dev <liran.shani@sisense.com>
2015-07-14 12:21:01 +0300

    fix

--- logstash/recipes/config.rb
+++ logstash/recipes/config.rb
@@ -1,5 +1,9 @@
-cookbook_file 'settings.conf' do
-	mode '0644'
-	path "/opt/logstash/server/etc/conf.d/settings.conf"
-	action :create_if_missing
+#cookbook_file 'settings.conf' do
+	#mode '0644'
+	#path "/opt/logstash/server/etc/conf.d/settings.conf"
+	#action :create_if_missing
+#end
+
+template "/opt/logstash/server/etc/conf.d/settings.conf" do
+  source "settings.erb"
 end
